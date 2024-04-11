7b73fbc060f22a609c5bfcb42e429cb027a7ee0d
clases <clases@Servidor.localdomain>
2024-04-11 09:54:48 +0200

    actualizo los app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+""" Docstring del módulo """
+
+
+def suma(a: int, b: int) -> int:
+    """ docstring function
+    # debe dar errores en linter
+    """
+    x = "no se usa"
+    y = "no se usa"
+    return a+ b
